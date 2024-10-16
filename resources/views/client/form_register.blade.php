9ee94c2f715d4876d7e1cf5d45c028699c1af6f8
Hau230624 <nihonggo2004@gmail.com>
2024-10-16 21:29:43 +0700

    ok

--- resources/views/client/form_register.blade.php
+++ resources/views/client/form_register.blade.php
@@ -67,7 +67,7 @@ register
                             <input type="text" name="email" id="email-field" class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder="Email">
                         </div>
                         <div class="mb-3">
-                            <label for="username-field"  class="inline-block mb-2 text-base font-medium">UserName</label>
+                            <label for="username-field"  class="inline-block mb-2 text-base font-medium">User Name</label>
                             <input type="text" id="username-field" name="name" class="form-input border-slate-200 dark:border-zink-500 focus:outline-none focus:border-custom-500 disabled:bg-slate-100 dark:disabled:bg-zink-600 disabled:border-slate-300 dark:disabled:border-zink-500 dark:disabled:text-zink-200 disabled:text-slate-500 dark:text-zink-100 dark:bg-zink-700 dark:focus:border-custom-800 placeholder:text-slate-400 dark:placeholder:text-zink-200" placeholder=" username">
                         </div>
                         <div class="mb-3">
