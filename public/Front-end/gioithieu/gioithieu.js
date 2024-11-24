06ab389abe30abbc06a674612175dae0d306ad8f
Hau230624 <nihonggo2004@gmail.com>
2024-11-24 20:32:59 +0700

    gioi thieu

--- public/Front-end/gioithieu/gioithieu.js
+++ public/Front-end/gioithieu/gioithieu.js
@@ -5,26 +5,26 @@ function createHTMLStructure() {
   
 
   // Tạo phần Banner
-  const banner = document.createElement("section");
-  banner.classList.add("banner");
-  banner.innerHTML = `
-    <div class="banner-container">
-      <!-- Thêm các slide banner vào đây -->
-      ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-        .map(
-          (i) =>
-            `<div class="banner-slide"><img src="../anh/banner/banner${i}.jpg" alt="Banner ${i}" /></div>`
-        )
-        .join("")}
-    </div>
-  `;
-  document.body.appendChild(banner);
+  // const banner = document.createElement("section");
+  // banner.classList.add("banner");
+  // banner.innerHTML = `
+  //   <div class="banner-container">
+  //     <!-- Thêm các slide banner vào đây -->
+  //     ${[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  //       .map(
+  //         (i) =>
+  //           `<div class="banner-slide"><img src="../anh/banner/banner${i}.jpg" alt="Banner ${i}" /></div>`
+  //       )
+  //       .join("")}
+  //   </div>
+  // `;
+  // document.body.appendChild(banner);
 
   // Tạo phần About Us
   const aboutUs = document.createElement("section");
   aboutUs.classList.add("about-us");
   aboutUs.innerHTML = `
-    <img src="../anh/aeboutique.webp" alt="AE Boutique" />
+    <img src="/Front-end/anh/aeboutique.webp" alt="AE Boutique" />
     <ul>
       <li>      AE Boutique là một cửa hàng thời trang chuyên về quần áo, với phong cách trẻ trung, hiện đại và đa dạng. Cửa hàng này hướng đến đối tượng khách hàng yêu thích sự sáng tạo, phong cách và cá tính trong việc lựa chọn trang phục. AE Boutique có thể cung cấp nhiều dòng sản phẩm từ quần áo thường ngày, trang phục công sở cho đến những bộ cánh dự tiệc sang trọng.,
 </li>
@@ -37,38 +37,38 @@ function createHTMLStructure() {
   document.body.appendChild(aboutUs);
 
   // Tạo phần Footer
-  const footer = document.createElement("footer");
-  footer.innerHTML = `
-    <hr />
-    <div class="footer-container">
-      <div class="footer-item">
-        <h5>Shop AE Boutique</h5>
-        <h6>Shop AE Boutique chuyên buôn, sỉ, lẻ, order hàng thời trang...</h6>
-        <div class="social-icons">
-          <a href="https://www.facebook.com/aeboutique69"><img src="../anh/fb.png" alt="Facebook" /></a>
-          <a href="#"><img src="../anh/youtube.png" alt="YouTube" /></a>
-        </div>
-      </div>
-      <div class="footer-item">
-        <h5>Liên kết nhanh</h5>
-        <ul>
-          <li><a href="#">Hướng dẫn đặt hàng</a></li>
-          <li><a href="#">Hướng dẫn thanh toán</a></li>
-          <li><a href="#">Chính sách đổi trả hàng</a></li>
-          <li><a href="#">Liên hệ với chúng tôi</a></li>
-        </ul>
-      </div>
-      <div class="footer-item">
-        <h5>Hotline hỗ trợ 24/7</h5>
-        <div class="hotline"><span>0975986096</span></div>
-        <h5>Chấp nhận thanh toán</h5>
-        <div class="payment-icons">
-          <img src="../anh/momo.png" alt="Momo" />
-        </div>
-      </div>
-    </div>
-  `;
-  document.body.appendChild(footer);
+  // const footer = document.createElement("footer");
+  // footer.innerHTML = `
+  //   <hr />
+  //   <div class="footer-container">
+  //     <div class="footer-item">
+  //       <h5>Shop AE Boutique</h5>
+  //       <h6>Shop AE Boutique chuyên buôn, sỉ, lẻ, order hàng thời trang...</h6>
+  //       <div class="social-icons">
+  //         <a href="https://www.facebook.com/aeboutique69"><img src="/Front-end/anh/fb.png" alt="Facebook" /></a>
+  //         <a href="#"><img src="/Front-end/anh/youtube.png" alt="YouTube" /></a>
+  //       </div>
+  //     </div>
+  //     <div class="footer-item">
+  //       <h5>Liên kết nhanh</h5>
+  //       <ul>
+  //         <li><a href="#">Hướng dẫn đặt hàng</a></li>
+  //         <li><a href="#">Hướng dẫn thanh toán</a></li>
+  //         <li><a href="#">Chính sách đổi trả hàng</a></li>
+  //         <li><a href="#">Liên hệ với chúng tôi</a></li>
+  //       </ul>
+  //     </div>
+  //     <div class="footer-item">
+  //       <h5>Hotline hỗ trợ 24/7</h5>
+  //       <div class="hotline"><span>0975986096</span></div>
+  //       <h5>Chấp nhận thanh toán</h5>
+  //       <div class="payment-icons">
+  //         <img src="/Front-end/anh/momo.png" alt="Momo" />
+  //       </div>
+  //     </div>
+  //   </div>
+  // `;
+  // document.body.appendChild(footer);
 }
 
 // Gọi hàm để tạo cấu trúc HTML
